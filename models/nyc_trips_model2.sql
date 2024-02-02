8734eaccc3a9abf15563b228a4e6b2bed1eb5aa1
Anshul Khandelwal <anshulkhandelwal94@gmail.com>
2024-02-02 09:04:54 +0530

    clickhouse model and sources

--- models/nyc_trips_model2.sql
+++ models/nyc_trips_model2.sql
@@ -1,13 +1,16 @@
+-- @materialize: true
 SELECT 
-       trip.request_datetime,
-       trip.trip_miles,
-       trip.trip_time,
-       trip.driver_pay,
-       trip.tips,
+       trip.request_datetime as request_datetime,
+       trip.trip_miles as trip_miles,
+       trip.trip_time as trip_time,
+       trip.driver_pay as driver_pay,
+       trip.tips as tips,
        pickup.Zone as pickup_zone,
        pickup.Borough as pickup_borough,
-       dropoff.Zone as dropoff_zone,
-       dropoff.Borough as dropoff_borough,
-FROM nyc_trips trip
-JOIN zones pickup ON (trip.PULocationID = pickup.LocationID)
-JOIN zones dropoff ON (trip.DOLocationID = dropoff.LocationID)
+       drop.Zone as dropoff_zone,
+       drop.Borough as dropoff_borough
+FROM {{ ref "nyc_trips" }} trip
+JOIN {{ ref "zones" }} pickup
+ON trip.PULocationID = pickup.LocationID
+JOIN zones drop
+ON trip.PULocationID = drop.LocationID
